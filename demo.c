3df4a3393968bc737432bd9459bad329d07b2b59
omt8861 <omt8861@gmail.com>
2026-01-02 15:41:44 +0530

    new file

--- demo.c
+++ demo.c
@@ -9,7 +9,7 @@ int main(){
     if(a>b){
         printf("%d is greater than %d", a, b);
     }else{
-        printf("%d is greater than %d", b, a);
+        printf("%d is less than %d", b, a);
     }
     return 0;
     
